1aab642cfac845799c56af5945ddadf37e0255e1
RustamGimadiev <gimadiev.kzn@yandex.ru>
2020-11-20 21:04:20 +0300

    example change

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -9,30 +9,30 @@ module "sak_kubeflow" {
 
   owner      = "provectus"
   repository = "sak-kubeflow"
-  branch     = "example"
+  branch     = "master"
 
   #Main route53 zone id if exist (Change It)
-  mainzoneid = "Z02149423PVQ0YMP19F13"
+  mainzoneid = "XXXXXXXXXXXXXXXXXXXXX"
 
   # Name of domains (create route53 zone and ingress). Set as array, first main ingress fqdn ["example.com", "example.io"]
-  domains = ["simple.edu.provectus.io"]
+  domains = ["simple.xxx.provectus.io"]
 
   # ARNs of users which would have admin permissions. (Change It)
   admin_arns = [
     {
-      userarn  = "arn:aws:iam::245582572290:user/rgimadiev"
+      userarn  = "arn:aws:iam::xxxxxxxxxxxx:user/rxxxxxxxv"
       username = "rgimadiev"
       groups   = ["system:masters"]
     }
   ]
 
   # Email that would be used for LetsEncrypt notifications
-  cert_manager_email = "rgimadiev@provectus.com"
+  cert_manager_email = "rxxxxxxxv@provectus.com"
 
   cognito_users = [
     {
-      email    = "rgimadiev@provectus.com"
-      username = "rgimadiev"
+      email    = "rxxxxxxxv@provectus.com"
+      username = "rxxxxxxxv"
       group    = "administrators"
     }
   ]
