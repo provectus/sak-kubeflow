5c344d52d0bb25f24e454191b4e8643df73e437d
akastav <switch@bk.ru>
2021-05-06 12:21:03 +0300

    add apps

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,34 +1,34 @@
 module "sak_kubeflow" {
   source = "../.."
 
-  cluster_name = "simple"
+  cluster_name = "testcognito"
 
   owner      = "provectus"
   repository = "sak-kubeflow"
-  branch     = "init"
+  branch     = "test"
 
   #Main route53 zone id if exist (Change It)
-  mainzoneid = "XXXXXXXXXXXXXXXXXXXXX"
+  mainzoneid = "Z04917561CQAI9UAF27D6"
 
   # Name of domains (create route53 zone and ingress). Set as array, first main ingress fqdn ["example.com", "example.io"]
-  domains = ["simple.xxx.provectus.io"]
+  domains = ["testcognito.sak.ninja"]
 
   # ARNs of users which would have admin permissions. (Change It)
-  admin_arns = [
-    {
-      userarn  = "arn:aws:iam::xxxxxxxxxxxx:user/rxxxxxxxv"
-      username = "rxxxxxxxv"
-      groups   = ["system:masters"]
-    }
-  ]
+  admin_arns = []
+  #   {
+  #     userarn  = "arn:aws:iam::xxxxxxxxxxxx:user/rxxxxxxxv"
+  #     username = "rxxxxxxxv"
+  #     groups   = ["system:masters"]
+  #   }
+  # ]
 
   # Email that would be used for LetsEncrypt notifications
-  cert_manager_email = "rxxxxxxxv@provectus.com"
+  cert_manager_email = "dkharlamov@provectus.com"
 
   cognito_users = [
     {
-      email    = "rxxxxxxxv@provectus.com"
-      username = "rxxxxxxxv"
+      email    = "dkharlamov@provectus.com"
+      username = "dkharlamov"
       group    = "administrators"
     }
   ]
