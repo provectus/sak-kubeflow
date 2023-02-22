080bae297b21cd1131d6e5f81cfaf10c4951da40
Pavlo Bunakalia <pblo@tuta.io>
2023-02-21 22:23:50 -0500

    run tflint recursively, add version constraints

--- kmeans-mnist-pipeline/versions.tf
+++ kmeans-mnist-pipeline/versions.tf
@@ -2,7 +2,6 @@ terraform {
   required_version = ">= 1.1"
   required_providers {
     aws        = ">= 2.66"
-    helm       = ">= 1.1.1"
     kubernetes = ">= 1.12" 
     null       = ">= 3.0"
   }
